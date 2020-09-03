0d2ae2dceb2e12e509c1990b9c176fc7bbbc3c56
Tomer Rubinstein <tomerrub11@gmail.com>
2020-09-03 16:48:15 +0300

    Now: Fully Working

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 from datetime import date
+from api import Currency
+
 
 class Application(Frame):
     def __init__(self, master=None):
@@ -10,21 +12,22 @@ class Application(Frame):
         self.createWidgets()
 
     def show_codes(self):
-        msg = messagebox.showinfo("Currency Codes", """
-        01	דולר​	ארצות הברית​
-        02​	לירה​	בריטניה​
-        03​	כתר​	שוודיה​
-        05​	פרנק​	שוויץ​
-        06​	דולר​	קנדה​
-        12​	כתר​	דנמרק​
-        17​	רנד​	דרום אפריקה​
-        18​	דולר​	אוסטרליה​
-        27​	אירו​	האיחוד המוניטרי האירופי​
-        28​	כתר​	נורווגיה​
-        31​	יין​	יפן​
-        69​	דינר​	ירדן (שטרי כסף)​
-        70​	לירה​	לבנון (שטרי כסף)​
-        79​	לירה​	מצרים (שטרי כסף)
+        messagebox.showinfo("Currency Codes", """
+            CODE        NAME           STATE
+            01	דולר​	ארצות הברית​
+            02​	לירה​	בריטניה​
+            03​	כתר​	שוודיה​
+            05​	פרנק​	שוויץ​
+            06​	דולר​	קנדה​
+            12​	כתר​	דנמרק​
+            17​	רנד​	דרום אפריקה​
+            18​	דולר​	אוסטרליה​
+            27​	אירו​	האיחוד המוניטרי האירופי​
+            28​	כתר​	נורווגיה​
+            31​	יין​	יפן​
+            69​	דינר​	ירדן (שטרי כסף)​
+            70​	לירה​	לבנון (שטרי כסף)​
+            79​	לירה​	מצרים (שטרי כסף)
         """)
 
     def createWidgets(self):
@@ -52,13 +55,28 @@ class Application(Frame):
         example_text = Label(self.master, text="i.e. 30/11/2005", font=("Fixedsys", 16))
         example_text.place(relx = 0.72, rely = 0.45, anchor=CENTER)
 
+        def api_interaction():
+            code = str(c_code.get())
+            _date = str(date_entry.get())
+
+            _date_parts = _date.split("/")
+            _date_parts[0], _date_parts[2] = _date_parts[2], _date_parts[0]
+            _date_parts = "".join(x for x in _date_parts)
+            
+            USD = Currency(code, _date_parts)
+            res = USD.get_value(USD.get_currency())
+            result_text = Label(self.master, text=res, font=("Fixedsys", 16))
+            result_text.place(relx = 0.55, rely = 0.55, anchor=CENTER)
+            # return res
+        
         # -= Search Button =-
-        search_btn = Button(self.master, text="Search", font=("Fixedsys", 12))
+        search_btn = Button(self.master, text="Search", font=("Fixedsys", 12), command=api_interaction)
         search_btn.place(relx = 0.55, rely = 0.50, anchor=CENTER)
         
 # WINDOW LOOP START
 root = Tk()
-
+root.resizable(0, 0)
+root.iconbitmap("assets/shekel.ico")
 ws = root.winfo_screenwidth()
 hs = root.winfo_screenheight()
 
@@ -71,6 +89,7 @@ y = (hs/2) - (height/2)
 app = Application(master=root)
 root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 root.title("Currency API")
+
 
 app.mainloop()
 # WINDOW LOOP END
